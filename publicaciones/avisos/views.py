542d1037bf543cbeecb3fe1b6fd6c79f5073e36c
LechuMTZ <am178949@gmail.com>
2025-09-24 21:32:16 -0600

    Agrega vista y template de avisos vigentes, y urls del proyecto

---
+++ publicaciones/avisos/views.py
@@ -0,0 +1,19 @@
+
+from django.views.generic import ListView
+from django.utils.timezone import localdate
+from .models import Aviso
+
+class AvisosVigentesListView(ListView):
+    model = Aviso
+    template_name = "avisos/lista.html"  # ruta al template
+    context_object_name = "avisos"
+
+    def get_queryset(self):
+        hoy = localdate()  # fecha actual (aware de zona horaria)
+        return (
+            Aviso.objects
+            .filter(fecha_inicio__lte=hoy, fecha_fin__gte=hoy)
+            .order_by('-fecha_inicio')
+        )
+
+
